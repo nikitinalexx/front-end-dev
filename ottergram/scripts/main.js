d7927921ee701dd1c3a0bb2155244194d53e1ef4
Oleksii Nikitin <oleksiin@suntechinnovation.com>
2019-04-08 18:06:28 +0300

    more styles

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,6 +1,7 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
 
 function setDetails(imageUrl, titleText) {
   'use strict';
@@ -45,6 +46,11 @@ function initializeEvents() {
   'use strict';
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+}
+
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
 initializeEvents();
