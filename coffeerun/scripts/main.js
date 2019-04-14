717819ae4478ac58b69fbea55d531cebc448be5d
nikitinalexx <nikitinalexx22@gmail.com>
2019-04-15 00:47:03 +0300

    silver coffeerun

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -1,8 +1,24 @@
 (function(window) {
   'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var STRENGTH_INPUT_SELECTOR = '[id="strengthLevel"]';
+  var STRENGTH_LEVEL_LABEL='[id="strengthLevelLabel"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
+  var SliderHandler = App.SliderHandler;
   var myTruck = new Truck('EAL', new DataStore());
   window.myTruck = myTruck;
+  var formHandler = new FormHandler(FORM_SELECTOR);
+  var sliderHandler = new SliderHandler(STRENGTH_INPUT_SELECTOR, STRENGTH_LEVEL_LABEL);
+
+  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  sliderHandler.addChangeHandler();
+
+  $(($(FORM_SELECTOR))[0]).on('reset', function() {
+    sliderHandler.reset();
+  })
+
+  console.log(formHandler);
 })(window);
---
+++ coffeerun/scripts/sliderhandler.js
@@ -0,0 +1,65 @@
+(function(window) {
+  'use strict';
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+
+
+  function SliderHandler(selector, label) {
+    if (!selector) {
+      throw new Error('No selector provided');
+    }
+    if (!label) {
+      throw new Error('No label provided');
+    }
+
+    this.$sliderElement = $(selector);
+    this.$sliderLabel = $(label);
+
+    if (this.$sliderElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+    if (this.$sliderLabel.length === 0) {
+      throw new Error('Could not find label with selector: ' + label);
+    }
+
+    this.defaultLabel = this.$sliderLabel[0].textContent + ' ' + this.$sliderElement[0].value;
+    this.defaultColor = $('.colorLabel').css('color');
+
+    this.$sliderLabel[0].textContent = this.$sliderLabel[0].textContent + ' ' + this.$sliderElement[0].value;
+
+    var labelSplit = this.$sliderLabel[0].textContent.split(' ');
+
+
+    delete labelSplit[labelSplit.length - 1];
+
+    this.initialLabelValue = labelSplit.join(' ');
+
+  }
+
+
+
+  SliderHandler.prototype.addChangeHandler = function() {
+    console.log('Setting changle handler for slider');
+    this.$sliderElement.on('input', function(data, event) {
+      this.$sliderLabel[0].textContent = this.initialLabelValue + ' ' + this.$sliderElement[0].value;
+      var color = this.defaultColor;
+      if (this.$sliderElement[0].value <= 50) {
+        var percent = this.$sliderElement[0].value / 50;
+        color = 'rgb(' + (255 * percent) + ', 255, 0';
+      } else {
+        var percent = (this.$sliderElement[0].value - 50) / 50;
+        color = 'rgb(255,' + (255 * (1 - percent)) + ', 0';
+      }
+      this.$sliderLabel[0].style.color = color;
+    }.bind(this))
+  };
+
+  SliderHandler.prototype.reset = function() {
+    this.$sliderLabel[0].textContent = this.defaultLabel;
+    this.$sliderLabel[0].style.color = this.defaultColor;
+  };
+
+  App.SliderHandler = SliderHandler;
+  window.App = App;
+})(window);
