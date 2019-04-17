25bb32657c1884fa932b8c2d5a84eed69bb14b10
Oleksii Nikitin <oleksiin@suntechinnovation.com>
2019-04-17 18:28:05 +0300

    complete coffeeerun

--- coffeerun/scripts/checklist.js
+++ coffeerun/scripts/checklist.js
@@ -14,8 +14,62 @@
   }
 
   function Row(coffeeOrder) {
-    // Здесь будет находиться код конструктора
+    var $div = $('<div></div>', {
+      'data-coffee-order': 'checkbox',
+      'class': 'checkbox'
+    })
+
+    var $label = $('<label></label>');
+
+    var $checkbox = $('<input></input>', {
+      type: 'checkbox',
+      value: coffeeOrder.emailAddress
+    });
+
+    var description = coffeeOrder.size + ' ';
+
+    if (coffeeOrder.flavor) {
+      description += coffeeOrder.flavor + ' ';
+    }
+
+    description += coffeeOrder.coffee;
+    if (coffeeOrder.extraAbility) {
+      description += ' ' + coffeeOrder.extraAbility;
+    }
+
+    description += ', (' + coffeeOrder.emailAddress + ')';
+    description += ' [' + coffeeOrder.strength + 'x]';
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+
+    this.$element = $div;
   }
+
+  CheckList.prototype.addRow = function(coffeeOrder) {
+    this.removeRow(coffeeOrder.emailAddress);
+
+    // Создаем новый экземпляр строки на основе информации о заказе кофе
+    var rowElement = new Row(coffeeOrder);
+    // Добавляем свойство $element нового экземпляра строки в перечень
+    this.$element.append(rowElement.$element);
+  };
+
+  CheckList.prototype.removeRow = function(email) {
+    this.$element
+      .find('[value="' + email + '"]')
+      .closest('[data-coffee-order="checkbox"]')
+      .remove();
+  };
+
+  CheckList.prototype.addClickHandler = function(fn) {
+    this.$element.on('click', 'input', function(event) {
+      var email = event.target.value;
+      this.removeRow(email);
+      fn(email);
+    }.bind(this));
+  };
 
 
   App.CheckList = CheckList;
--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -7,19 +7,27 @@
   var MODAL_CONFIRM_SELECTOR = '[id="modalConfirm"]';
   var EMAIL_INPUT_SELECTOR = '[id="emailInput"]';
   var EXTRA_ABILITY_SELECTOR = '[id="extraAbilityDiv"]';
+  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
   var SliderHandler = App.SliderHandler;
   var EmailHandler = App.EmailHandler;
+  var CheckList = App.CheckList;
   var myTruck = new Truck('EAL', new DataStore());
   window.myTruck = myTruck;
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
   var formHandler = new FormHandler(FORM_SELECTOR, MODAL_SELECTOR, MODAL_CONFIRM_SELECTOR);
   var sliderHandler = new SliderHandler(STRENGTH_INPUT_SELECTOR, STRENGTH_LEVEL_LABEL);
   var emailHandler = new EmailHandler(EMAIL_INPUT_SELECTOR, EXTRA_ABILITY_SELECTOR);
 
-  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck), myTruck.makeUserWip.bind(myTruck), myTruck.getWip.bind(myTruck));
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+
+  formHandler.addSubmitHandler(function(data) {
+    myTruck.createOrder.bind(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  }, myTruck.makeUserWip.bind(myTruck), myTruck.getWip.bind(myTruck));
   sliderHandler.addChangeHandler();
   emailHandler.addChangeHandler(myTruck.getWip.bind(myTruck));
 
